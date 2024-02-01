ed7743f13aed8647a7a37e8872cf04f7b408662d
TemwaNyirenda <temwanyirenda00@gmail.com>
2024-02-02 01:38:29 +0200

    Donezo

--- public/main.js
+++ public/main.js
@@ -36,15 +36,12 @@ const analytics = getAnalytics(app);
 
 const db = getDatabase();
 
-// const form = document.querySelector('#contact_form')
 
 let fname = document.querySelector("#fname");
 let lname = document.querySelector("#lname");
 let email = document.querySelector("#email");
 let phone = document.querySelector("#phone");
 let message = document.querySelector("#message");
-
-console.log(fname + lname+ email);
 
 let submitBtn = document.querySelector("#submit");
 
@@ -89,7 +86,6 @@ function validateForm() {
 
 
 submitBtn.addEventListener('click', InsertData);
-// findBtn.addEventListener('click', FindData);
 
 
 
@@ -113,23 +109,187 @@ function topFunction() {
 
 mybutton.addEventListener('click', topFunction);
 
-// const fs = require('fs');
+/**
+ * 
+ * 
+ * For opening animation
+ * 
+ * 
+ */
 
-// function getAllData() {
-//     const dbRef = ref(db);
+const bouncingBall = document.getElementById("bouncingBall");
 
-//     get(child(dbRef, "Contact"))
-//     .then((snapshot) => {
-//         let contacts = [];
+        let screenWidth = window.innerWidth;
+        let screenHeight = window.innerHeight;
 
-//         snapshot.forEach(childSnapshot => {
-//             contacts.push(childSnapshot.val());
-//         });
-//     })
 
-//     fs.writeFileSync('~/Downloads/data.json', jsonString, 'utf-8', (err) => {
-//         if (err) throw err;
-//         console.log('Data added to file');
-//     });
-// }
+        let bouncingBallStartX = 0;
+        let bouncingBallStartY = screenHeight * 0.40;
 
+        let movingBallX = bouncingBallStartX;
+        let movingBallY = bouncingBallStartY;
+
+        let curveWidth = screenWidth * 0.1;
+        let curveHeight;
+
+        let numVerticalBalls;
+
+        let xSpeed = screenWidth * 0.0025;
+        let ySpeed;
+        let xAtCurveHeight;
+        let xAtScreenHeight;
+
+        let bouncingBallWidth = 50;
+        let bouncingBallHeight = 50;
+
+        let ballsCount = 1;
+
+        let nameOpacity = 0.25;
+
+        let colours = ["#acfaa3", "#a3d8fa", "#f0a3fa", "#faa3a4", "#faf0a3", "#c5a3fa"];
+
+
+
+        function solveForA(curveHeight, xAtCurveHeight, xAtScreenHeight) {
+            // Quadratic equation  y = a * (x - vertexX) * (x - vertexX) + vertexY
+            // Solve for a
+            // a = (y - vertexY) / ( (x - vertexX) * (x - vertexX))
+            return (screenHeight - 50 - curveHeight)/((xAtScreenHeight - xAtCurveHeight) ** 2);
+        }
+
+        function getY (aValue, xValue, curveHeight, xAtCurveHeight ) {
+            // Quadratic equation  y = a * (x - vertexX) * (x - vertexX) + vertexY
+            
+            return aValue * ((xValue - xAtCurveHeight) ** 2) + curveHeight;
+        }
+
+
+        function animate() {
+
+            if (movingBallX == 0) { // first curve
+                curveHeight = bouncingBallStartY;
+                xAtCurveHeight = 0;
+                xAtScreenHeight = screenWidth * 0.05;
+            }
+
+            if (curveHeight >= screenHeight) {
+                zoomBouncingBall();
+                return;
+            }
+
+            movingBallX += xSpeed;
+            movingBallY = getY(solveForA(curveHeight, xAtCurveHeight, xAtScreenHeight), movingBallX, curveHeight, xAtCurveHeight);
+
+
+            if (movingBallY + 50 >= screenHeight) {
+                curveHeight = curveHeight + bouncingBallStartY * 0.2;
+                xAtCurveHeight = xAtCurveHeight + screenWidth * 0.1;
+                xAtScreenHeight = xAtScreenHeight + screenWidth * 0.1;
+                movingBallY = screenHeight - 50;
+            }
+
+            bouncingBall.style.left = movingBallX+ "px";
+            bouncingBall.style.top = movingBallY+ "px";
+            requestAnimationFrame(animate);
+        }
+
+        function zoomBouncingBall() {
+            if (movingBallX <= 0 || movingBallY <= 0 ) {
+                document.body.style.backgroundColor = "#fac5a3"
+                insertName();
+                addBalls();
+                return;
+            }
+
+            bouncingBallWidth += 50;
+            bouncingBallHeight += 50;
+            movingBallX -= 50;
+            movingBallY -= 50;
+
+
+            bouncingBall.style.width = bouncingBallWidth + "px";
+            bouncingBall.style.height = bouncingBallHeight + "px";
+            bouncingBall.style.left = movingBallX + "px";
+            bouncingBall.style.top = movingBallY + "px";
+            requestAnimationFrame(zoomBouncingBall);
+        }
+
+        function insertName() {
+            let name = document.getElementById("name");
+
+            name.style.opacity = 0.25;
+
+            const nameContent = document.createTextNode("Temwa Nyirenda");
+
+            name.appendChild(nameContent);
+
+            
+            return;
+
+        }
+        
+        
+function addBalls() {
+            
+    if (colours.length === 0) {
+        return;
+    }
+
+            if (ballsCount >= 500) {
+                skipFunction()
+                return;
+            }
+
+            let sizes = [50, 100, 250, 500]
+            const newBall = document.createElement("div");
+
+            newBall.setAttribute("class", "balls")
+            newBall.setAttribute("id", "ball" + ballsCount)
+            newBall.style.right = Math.floor(Math.random() * screenWidth) + "px";
+            newBall.style.bottom = Math.floor(Math.random() * screenHeight) + "px";
+
+            
+            newBall.style.backgroundColor = colours[Math.floor(Math.random() * 6)];
+            
+            let randomSize = sizes[Math.floor(Math.random() * 4)];
+            newBall.style.width =  randomSize + "px";
+            newBall.style.height = randomSize + "px";
+            
+            bouncingBall.style.opacity = 0;
+            document.body.insertBefore(newBall, bouncingBall);
+            
+
+            let name = document.getElementById("name");
+            nameOpacity += 0.05;
+            name.style.opacity = nameOpacity;
+
+
+            ballsCount++;
+            requestAnimationFrame(addBalls);
+        }
+
+        function skipFunction() {
+            let name = document.getElementById("name");
+            let skipAnimation = document.getElementById("skipAnimation");
+            let balls = document.getElementsByClassName("balls");
+
+            name.style.display = "none";
+            bouncingBall.style.display = "none";
+            skipAnimation.style.display = "none";
+
+            colours = [];
+
+             for (let i = 0; i < balls.length; i++) {
+                balls[i].style.display = "none";
+            }
+
+            let cv = document.getElementById("cv");
+            cv.style.display = "block";
+
+            } 
+let skipAnimationBtn = document.getElementById("skipAnimation");
+skipAnimationBtn.addEventListener('click', skipFunction);
+
+
+
+        animate();
